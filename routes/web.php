a5bd42c29548c100ed0759bc27df2e0a94fd4ba1
Hamza <penjohamza193@gmail.com>
2024-04-22 15:47:24 +0200

    Napravio CRUD

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Practitioner\PracticeController;
+use App\Http\Controllers\Practitioner\PostClientController;
 use App\Http\Controllers\Client\DashboardController as ClientDashboardController;
 use App\Http\Controllers\Practitioner\DashboardController as PractitionerDashboardController;
 use App\Http\Controllers\Practitioner\PractitionerClientController as PractitionerClientController;
@@ -21,8 +22,14 @@ Route::get('/dashboard', function () {
 Route::prefix('practitioners')->middleware(['auth', 'practitioner'])->group(function () {
     Route::get('/dashboard', [PractitionerDashboardController::class, 'index'])->name('practitioner-dashboard');
     Route::get('/clients', [PractitionerClientController::class, 'showClients'])->name('practitioner-clients');
-    Route::get('/clients/{id}', [PractitionerClientController::class, 'showSingleClient'])->name('client-show')->middleware(['auth', 'practitioner']);
+    Route::get('/clients/add-client', [PractitionerClientController::class, 'addClient'])->name('add-client')->middleware(['auth', 'practitioner']);
+    Route::post('/clients/store-client', [PractitionerClientController::class, 'storeClient'])->name('store-client')->middleware(['auth', 'practitioner']);
+    Route::get('/clients/{id}/edit-client', [PractitionerClientController::class, 'editClient'])->name('edit-client')->middleware(['auth', 'practitioner']);
+    Route::put('/clients/{id}/update-client', [PractitionerClientController::class, 'storeEdit'])->name('store-edit')->middleware(['auth', 'practitioner']);
+    Route::delete('/clients/{id}/delete-client', [PractitionerClientController::class, 'deleteClient'])->name('delete-client')->middleware(['auth', 'practitioner']);
     Route::get('/practices', [PracticeController::class, 'showPractice'])->name('practice')->middleware('auth', 'practitioner');
+    Route::get('/clients/{id}', [PractitionerClientController::class, 'showSingleClient'])->name('client-show')->middleware(['auth', 'practitioner']);
+
 });
 Route::get('/client-dashboard', [ClientDashboardController::class, 'index'])->name('client-dashboard')->middleware(['auth', 'client']);
 
